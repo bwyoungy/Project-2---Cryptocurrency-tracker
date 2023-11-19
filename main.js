657b3c1da70fa3af23a4a799f606dd56efd239f8
bwyoungy <youngyaacov@gmail.com>
2023-11-19 15:52:28 +0200

    implemented linking between frames

--- main.js
+++ main.js
@@ -5,4 +5,19 @@
     // show home frame on load
     document.getElementById("homeFrame").style.display="block";
 
+    let validationObjs = document.getElementsByClassName("taskInputField");
+    
+    // Iterate over link objects to the frames - marked in HTML with class="frameLink"
+    for (const aLink of document.getElementsByClassName("frameLink")) {
+        // for each link object add a click event with a function to show only that frame
+        aLink.addEventListener("click", function(){
+            // hide all frames
+            for (const frame of document.getElementsByClassName("frame")) {
+                frame.style.display="none";
+            }
+            // show the frame based on the link clicked (alink data-frame corresponds to section id)
+            document.getElementById(this.dataset.frame).style.display="block";
+        });
+    }
+
 })()
